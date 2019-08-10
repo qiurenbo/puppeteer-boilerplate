c4ac9252c8dda0c0861e89cbec0684b9a4841dbd
qiurenbo <qiurenbo@outlook.com>
2019-08-10 14:24:37 +0800

    feat: add vscode debug config
    1. add vscode debug config
    2. add moment
    3, remove unuseful code

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
 'use strict';
-const sleep = require('sleep-while');
+const moment = require('moment');
 const puppeteer = require('puppeteer');
 
-let pageTarget;
-let clickAndWaitForTarget = async (clickSelector, page, browser) => {
-	pageTarget = page.target(); //save this to know that this was the opener
-	await page.click(clickSelector); //click on a link
-	const newTarget = await browser.waitForTarget(target => target.opener() === pageTarget); //check that you opened this page, rather than just checking the url
-	const newPage = await newTarget.page(); //get the page object
-	// await newPage.once("load",()=>{}); //this doesn't work; wait till page is loaded
-	await newPage.waitForSelector('body'); //wait for page to be loaded
-
-	await newPage.setViewport({
-		width: 1920,
-		height: 1080,
-	});
-
-	return newPage;
-};
-
 (async () => {
-	// lauch
 	let launchOptions = {};
 	launchOptions.headless = false;
 	launchOptions.slowMo = 100;
@@ -38,6 +20,8 @@ let clickAndWaitForTarget = async (clickSelector, page, browser) => {
 	// go to index
 	await page.goto('https://www.google.com');
 
-	await sleep(1000);
 	browser.close();
+
+	const now = moment().format('MMMM Do YYYY, h:mm:ss a');
+	console.log(`${now}`);
 })();
