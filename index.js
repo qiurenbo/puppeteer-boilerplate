8fffb2a2bd5e95f6a9edc3d24ce1acfd584253d8
qiurenbo <qiurenbo@outlook.com>
2019-08-21 08:56:55 +0800

    feat : add argv userDataDir

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 const moment = require('moment');
 const puppeteer = require('puppeteer');
-
+const argv = require('yargs').argv;
 (async () => {
 	let launchOptions = {};
 	launchOptions.headless = false;
 	launchOptions.slowMo = 100;
 	launchOptions.args = ['--window-size=1920,1080'];
 	launchOptions.executablePath = 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe';
-
+	launchOptions.userDataDir = argv.userDataDir;
 	let browser = await puppeteer.launch(launchOptions);
 	let page = await browser.newPage();
 
